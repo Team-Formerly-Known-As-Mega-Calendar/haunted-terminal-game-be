d76fcda901d9fe524c39a5eb56c81b42bc79f207
Michelle Stermitz <stermitzm@gmail.com>
2020-10-02 13:36:06 -0700

    Added user insert with create

--- data/data-helpers.js
+++ data/data-helpers.js
@@ -3,12 +3,6 @@ const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('haunted-terminal-game routes', () => {
-  beforeEach(() => {
+beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
-  });
-
-  it('adding dummy test to remove fail message', () => {    
-    expect('yes').toEqual('yes');  
-  });
 });
