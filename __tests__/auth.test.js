9241750dcf51f4eb2ccb235c7c4b3b72f9c6d25b
Thomas Stussi <tstussi06@gmail.com>
2020-10-02 13:45:34 -0700

    login test

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,13 +2,28 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('auth routes', () => {
-  it('signs up a new user via POST and returns user', async() => {
+  it('signs up a new user via POST and returns user', async () => {
     const response = await request(app)
       .post('/api/v1/auth/signup')
       .send({
         email: 'test@test.com',
         password: 'password',
         name: 'Bonzi'
+      });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'test@test.com',
+      name: 'Bonzi'
+    });
+  });
+
+  it('logs in a user via POST', async () => {
+    const response = await request(app)
+      .post('/api/v1/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: 'password'
       });
 
     expect(response.body).toEqual({
