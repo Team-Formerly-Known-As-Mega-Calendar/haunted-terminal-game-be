4662da72525aa6eaf4b0ce83ae2c84344386a428
SarahRector <65381642+SarahRector@users.noreply.github.com>
2020-10-05 15:40:07 -0700

    Story table (#5)
    * readme.md (#2)
    
    * Added user insert with create
    
    * login test
    
    * auth login test
    
    * salt 3
    
    * node ci fix
    
    * auth tests working
    
    * story insert sans test
    
    * added test for post route
    
    * find by storyId
    
    * staging changes
    
    * story->stage
    
    * update route
    
    * stash
    
    * added delete route and test
    
    Co-authored-by: Thomas R Stussi <63024117+Thomas-Stussi@users.noreply.github.com>
    Co-authored-by: Michelle Stermitz <stermitzm@gmail.com>
    Co-authored-by: Thomas Stussi <tstussi06@gmail.com>
    Co-authored-by: ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,18 @@
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS stages CASCADE;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL,
   name TEXT NOT NULL
+);
+
+CREATE TABLE stages (
+  stage_id TEXT PRIMARY KEY,
+  name TEXT NOT NULL,
+  message TEXT NOT NULL,
+  choices JSONB NOT NULL,
+  img TEXT,
+  sound TEXT
 );
