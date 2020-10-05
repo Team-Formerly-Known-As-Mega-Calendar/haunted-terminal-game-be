b91cd9915067b361f94168d44b33a765580a35dd
Thomas Stussi <tstussi06@gmail.com>
2020-10-05 14:31:06 -0700

    story->stage

--- __tests__/stage.test.js
+++ __tests__/stage.test.js
@@ -1,50 +1,50 @@
 const request = require('supertest');
 const app = require('../lib/app');
-const Story = require('../lib/models/story');
+const Stage = require('../lib/models/stage');
 require('../data/data-helpers');
 
 
-describe('story routes', () => {
-  it('creates a new story via POST and returns story', async() => {
+describe('stage routes', () => {
+  it('creates a new stage via POST and returns stage', async() => {
     return request(app)
-      .post('/api/v1/stories')
+      .post('/api/v1/stage')
       .send({
-        storyId: '1-origin',
+        stageId: '1-origin',
         name: 'D.O.M.',
         message: 'This is 1-origin node',
-        choices: JSON.stringify(['choice 1', 'choice 2']),
+        choices: JSON.stringify([{ prompt: 'choice 1', next: '1-A1' }, { prompt: 'choice 2', next: '1-A2' }]),
         img: 'placekitten.com/200/200',
         sound: 'https://www.audioblocks.com/stock-audio/crescendo-reverse-piano-low-be3xmjh2uwrk0wxu6ku.html',
       })
 
       .then (res => {
         expect(res.body).toEqual({
-          storyId: '1-origin',
+          stageId: '1-origin',
           name: 'D.O.M.',
           message: 'This is 1-origin node',
-          choices: ['choice 1', 'choice 2'],
+          choices: [{ prompt: 'choice 1', next: '1-A1' }, { prompt: 'choice 2', next: '1-A2' }],
           img: 'placekitten.com/200/200',
           sound: 'https://www.audioblocks.com/stock-audio/crescendo-reverse-piano-low-be3xmjh2uwrk0wxu6ku.html',
         });
       });
   });
 
-  it('gets a story by its storyId via GET', async() => {
-    const story = await Story.insert({
-      storyId: '1-origin',
+  it('gets a stage by its stageId via GET', async() => {
+    const stage = await Stage.insert({
+      stageId: '1-origin',
       name: 'D.O.M.',
       message: 'This is 1-origin node',
-      choices: JSON.stringify(['choice 1', 'choice 2']),
+      choices: JSON.stringify([{ prompt: 'choice 1', next: '1-A1' }, { prompt: 'choice 2', next: '1-A2' }]),
       img: 'placekitten.com/200/200',
       sound: 'https://www.audioblocks.com/stock-audio/crescendo-reverse-piano-low-be3xmjh2uwrk0wxu6ku.html',
     });
-    return request(app).get(`/api/v1/stories/${story.storyId}`)
+    return request(app).get(`/api/v1/stage/${stage.stageId}`)
       .then(res => {
         expect(res.body).toEqual({
-          storyId: '1-origin',
+          stageId: '1-origin',
           name: 'D.O.M.',
           message: 'This is 1-origin node',
-          choices: ['choice 1', 'choice 2'],
+          choices: [{ prompt: 'choice 1', next: '1-A1' }, { prompt: 'choice 2', next: '1-A2' }],
           img: 'placekitten.com/200/200',
           sound: 'https://www.audioblocks.com/stock-audio/crescendo-reverse-piano-low-be3xmjh2uwrk0wxu6ku.html',
         });
