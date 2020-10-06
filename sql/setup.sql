35ecdc68d56b00f02f07ef343f6c128eb7c4051c
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-10-06 16:57:46 -0700

    fixed stages tests

--- sql/setup.sql
+++ sql/setup.sql
@@ -9,15 +9,6 @@ CREATE TABLE users (
 
 CREATE TABLE stages (
   stage_id TEXT PRIMARY KEY,
-  name TEXT NOT NULL,
-  message TEXT NOT NULL,
-  choices JSONB NOT NULL,
-  img TEXT,
-  sound TEXT
-);
-
-CREATE TABLE stages (
-  stage_id TEXT PRIMARY KEY,
   message TEXT NOT NULL,
   choices JSONB NOT NULL,
   img TEXT,
