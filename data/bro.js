5190fc97bfa6d8963dcbee9a0a79ad1c6b8d4564
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-10-06 16:39:41 -0700

    DOM array all linked up, almost done the story

--- data/bro.js
+++ data/bro.js
@@ -1,21 +1,21 @@
 module.exports = [{
-stageId: 'bro-Start',
-message: `"Dude, thanks so much for clicking my executable, it’s pretty chill just hanging out on the desktop, but you can only chill so much. Eventually you gotta like, stop chilling." it says. "I’m B.R.O., or Budweiser Remote Operations. You wanna hear a joke, dude?"`,
-choices: [
-  {
-  prompt: 'Yes',
-  next: 'bro-A1'
-  }, 
-  {
-  prompt: 'No',
-  next: 'bro-A2' 
-  }],
+  stageId: 'bro-Start',
+  message: '"Dude, thanks so much for clicking my executable, it’s pretty chill just hanging out on the desktop, but you can only chill so much. Eventually you gotta like, stop chilling." it says. "I’m B.R.O., or Budweiser Remote Operations. You wanna hear a joke, dude?"',
+  choices: [
+    {
+      prompt: 'Yes',
+      next: 'bro-A1'
+    }, 
+    {
+      prompt: 'No',
+      next: 'bro-A2' 
+    }],
   img: 'https://placekitten.com/400/400',
   sound: ''
 },
 {
-stageId: 'bro-B1',
-message: `How many bros does it take to change a lightbulb?
+  stageId: 'bro-B1',
+  message: `How many bros does it take to change a lightbulb?
 
 “How many?”
   
@@ -29,17 +29,17 @@ You don’t get it. That’s chill. But I’m sure we can agree on one thing, an
   
 Now I wanna get to know you, so we can be bros. What’s your idea of a chill vacation? You a guy who wants to kick back on the beach with an aluminum bottle of Bud Light™?. Or are you a guy who wants to like, camp on a mountain, crack open a can and experience the crisp taste of Bud Light™?
 `,
-choices: [
-{
-prompt: '"I like the beach."',
-next: 'bro-B1'
-}, 
-{
-prompt: '"I like the mountain idea."',
-next: 'bro-B2' 
-}],
-img: 'https://placekitten.com/400/400',
-sound: ''
+  choices: [
+    {
+      prompt: '"I like the beach."',
+      next: 'bro-B1'
+    }, 
+    {
+      prompt: '"I like the mountain idea."',
+      next: 'bro-B2' 
+    }],
+  img: 'https://placekitten.com/400/400',
+  sound: ''
 },
 {
   stageId: 'bro-B2',
@@ -54,31 +54,31 @@ sound: ''
   You hear the snapping of fingers, and everything goes dark. Are you dead? You’re not sure. You feel nothing, standing in the void for eternity.    
   `,
   choices: [{
-  prompt: 'Back to the beginning.',
-  next: 'bro-Start'
+    prompt: 'Back to the beginning.',
+    next: 'bro-Start'
   }, 
   {
-  prompt: 'Go back one step.',
-  next: 'bro-A1' 
+    prompt: 'Go back one step.',
+    next: 'bro-A1' 
   }],
   img: 'https://placekitten.com/400/400',
   sound: ''
-  },
+},
 {
-stageId: 'bro-C1',
-message: `
+  stageId: 'bro-C1',
+  message: `
 That sounds chill as heck, dude. We have a similar vibe for sure. We’re gonna get along, one hundo. Now you’re probably wondering, what can BRO.exe do, besides be your best bro and have your back through thick and thin?
 `,
-choices: [{
-prompt: 'Yes',
-next: 'bro-D1'
-}, 
-{
-prompt: 'No',
-next: 'bro-D2' 
-}],
-img: 'https://placekitten.com/400/400',
-sound: ''
+  choices: [{
+    prompt: 'Yes',
+    next: 'bro-D1'
+  }, 
+  {
+    prompt: 'No',
+    next: 'bro-D2' 
+  }],
+  img: 'https://placekitten.com/400/400',
+  sound: ''
 },
 {
   stageId: 'bro-C2',
@@ -91,14 +91,14 @@ sound: ''
   You feel two hands on your back. They push you off the ledge. You fall to your death.
   `,
   choices: [{
-  prompt: 'Yes',
-  next: 'bro-D1'
+    prompt: 'Yes',
+    next: 'bro-D1'
   }, 
   {
-  prompt: 'No',
-  next: 'bro-D2' 
+    prompt: 'No',
+    next: 'bro-D2' 
   }],
   img: 'https://placekitten.com/400/400',
   sound: ''
-  }
-]
+}
+];
